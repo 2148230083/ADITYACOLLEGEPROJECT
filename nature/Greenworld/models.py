70fe67b58a3c0cf1bb4630ebab8486c0070e6e32
2148230083 <168507930+2148230083@users.noreply.github.com>
2024-04-30 10:31:04 +0530

    Add files via upload

---
+++ nature/Greenworld/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class nature(models.Model):
+    seasons=models.CharField(max_length=100)
+    weather=models.ImageField()
+    fruit=models.ImageField()
+    tempature=models.IntegerField()
+    habitat=models.ImageField()
+
+class flora(models.Model):
+    name=models.CharField(max_length=50)
+    pictures=models.ImageField()
+
+    def str(self):
+        return self.name
+
+class fauna(models.Model):
+    name=models.CharField(max_length=50)
+    livingstyle=models.ImageField()
+
+    def str(self):
+        return self.name
